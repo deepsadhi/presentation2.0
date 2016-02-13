c89b07f6ab1b5cf9e737ee59ed9298a382351e92
Deepak <deeps.adhi@gmail.com>
2016-02-13 20:29:40 +0545

    sqlite test started

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -1,11 +1,17 @@
 <?php
 
 defined('APP_PATH') ? null : define('APP_PATH', dirname(dirname(__FILE__)));
+defined('APP_ROOT') ? null : define('APP_ROOT', dirname(dirname(__FILE__)));
 
 require_once APP_PATH.'/vendor/autoload.php';
+require_once APP_PATH.'/bootstrap/define.php';
 
 use App\Session;
 use App\File;
+use App\Sqllite;
 
 $s = new Session;
+
+$db = Sqllite::getConnection();
+
 
