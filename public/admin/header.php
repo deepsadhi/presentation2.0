1156349f49882a9dd53c4a4201ac9b57be8e145f
Deepak <deeps.adhi@gmail.com>
2016-02-13 20:30:08 +0545

    assets path changed

--- public/admin/header.php
+++ public/admin/header.php
@@ -6,9 +6,9 @@
   <title>Presentation2.0</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <link rel="stylesheet" href="../css/vendor/bootstrap.css" media="screen">
-  <link rel="stylesheet" href="../css/custom.css">
-  <link rel="stylesheet" href="../css/main.css">
+  <link rel="stylesheet" href="css/vendor/bootstrap.css" media="screen">
+  <link rel="stylesheet" href="css/custom.css">
+  <link rel="stylesheet" href="css/main.css">
  </head>
   <body>
 
