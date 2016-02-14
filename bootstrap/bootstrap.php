dcec270e0b46b0a8d613a4f23b2e5ceb713c553a
Deepak <deeps.adhi@gmail.com>
2016-02-14 20:24:34 +0545

    sqlite check

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -1,7 +1,10 @@
 <?php
 
+error_reporting(E_ALL | E_STRICT);
+
 defined('APP_PATH') ? null : define('APP_PATH', dirname(dirname(__FILE__)));
 defined('APP_ROOT') ? null : define('APP_ROOT', dirname(dirname(__FILE__)));
+
 
 require_once APP_PATH.'/vendor/autoload.php';
 require_once APP_PATH.'/bootstrap/define.php';
