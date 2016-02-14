4806193d4bbee498ad6459a12b9329faa206593a
Deepak <deeps.adhi@gmail.com>
2016-02-14 20:20:06 +0545

    get and set value from table added

--- app/Database.php
+++ app/Database.php
@@ -3,86 +3,143 @@
 namespace App;
 
 use App\Sqllite;
+use \PDO;
+use \PDOException;
+use \RuntimeException;
 
-class DB
+class Database
 {
+    /**
+     * Database connection
+     * @var PDO object
+     */
     private $_db;
-    public $lastInsertId;
-    public $error = false;
 
+    /**
+     * Bind parameter
+     * @var PDO object
+     */
+    private $_stmt;
+
+    /**
+     * Number of rows of last query
+     * @var int
+     */
+    public $rowCount;
+
+    /**
+     * Query result
+     * @var array
+     */
+    public $result;
+
+    /**
+     * Create Sqlite connection
+     */
     public function __construct()
     {
         $this->_db = Sqllite::getConnection();
     }
 
-    public function insert($table, array $data)
+    /**
+     * Initialize query result to null
+     */
+    private function _initQuery()
     {
-        $keys = array();
-        foreach ($data as $key => $value)
+        $this->result = array();
+    }
+
+    /**
+     * Bind parameter
+     * @param  array  $data Data to be inserted to table
+     */
+    private function _bind(array $data)
+    {
+        foreach($data as $d)
         {
-            $keys[] = $key;
+            if ($d['type'] == 'INT')
+            {
+                $this->_stmt->bindValue(':'.$d['key'],
+                                        $d['value'],
+                                        PDO::PARAM_INT);
+            }
+            else if ($d['type'] == 'STR')
+            {
+                $this->_stmt->bindValue(':'.$d['key'],
+                                        $d['value'],
+                                        PDO::PARAM_STR);
+            }
+        }
+    }
+
+    /**
+     * Insert data to table
+     * @param  string $table Table name
+     * @param  array  $data  Data to be inserted in table
+     * @return bool   Success or failure to set value
+     */
+    public function setValueOf($table, array $data)
+    {
+        $this->_initQuery();
+
+        $keys = array();
+        foreach ($data as $d)
+        {
+            $keys[] = $d['key'];
         }
 
-        try {
-            $sql  = 'INSERT INTO '.$table.'('.implode(', ', $keys).') ';
-            $sql .= 'VALUES (:'.implode(', :', $keys).')';
+        try
+        {
+            $sql  = 'INSERT INTO '.$table.'('.implode(', ', $keys).') '.
+                    'VALUES (:'.implode(', :', $keys).')';
 
-            $stmt = $this->_db->prepare($sql);
-            foreach($data as $key => $value)
-            {
-                if ($key == 'r_id')
-                {
-                    $stmt->bindValue(':'.$key, $value, PDO::PARAM_INT);
-                }
-                else
-                {
-                    $stmt->bindValue(':'.$key, $value, PDO::PARAM_STR);
-                }
-            }
-            $stmt->execute();
-            $this->lastInsertId = $this->_db->lastInsertId();
+            $this->_stmt = $this->_db->prepare($sql);
+            $this->_bind();
+            $this->_stmt->execute();
 
             return true;
-        } catch (Exception $e) {
-            error_log($e->getMessage());
+        }
+
+        catch (PDOException $e)
+        {
+            throw new RuntimeException($e->getMessage(), 101);
+
             return false;
         }
     }
 
-    public function query($sql)
+    /**
+     * Get value of filed from a table
+     * @param  string $table Name of table
+     * @param  array  $data  Query data
+     * @return bool          Success or failure to get value
+     */
+    public function getValueOf($table, array $data)
     {
-        try {
-            $stmt = $this->_db->prepare($sql);
-            $stmt->execute();
+        $this->_initQuery();
 
-            $result = array();
-            $result['categories'] = $stmt->fetchAll(PDO::FETCH_COLUMN);
-
-            $stmt->execute();
-            $result['rows'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            $result['error'] = false;
-
-            return $result;
-        } catch (Exception $e) {
-            error_log($e->getMessage());
-            $result['error'] = true;
-            return $result;
-        }
-    }
-
-    public function findByCId($c_id)
-    {
         try
         {
-            $sql = 'SELECT * FROM email WHERE c_id=?';
-            $stmt = $this->_db->prepare($sql);
-            $stmt->bindValue(1, $c_id, PDO::PARAM_INT);
-            $stmt->execute();
-            return  $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $sql  = 'SELECT * FROM '.$table.' WHERE '.
+                    $data['key'].' = :'.$data['key'].' LIMIT 1';
+
+            $this->_stmt = $this->_db->prepare($sql);
+            $this->_bind(array($data));
+            $this->_stmt->execute();
+            $this->result   = $this->_stmt->fetchAll(PDO::FETCH_ASSOC);
+            $this->rowCount = count($this->result);
+            if ($this->rowCount > 0)
+            {
+                $this->result = array_shift($this->result);
+            }
+
+            return true;
         }
-        catch (Exception $e)
+
+        catch (PDOException $e)
         {
-            error_log($e->getMessage());
+            throw new RuntimeException($e->getMessage(), 102);
+
             return false;
         }
     }
