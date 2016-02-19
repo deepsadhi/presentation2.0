45dfedb5195a59e9c7bf05ccc5bbdb78b80746e7
Deepak <deeps.adhi@gmail.com>
2016-02-19 22:14:04 +0545

    twig started

--- app/dependencies.php
+++ app/dependencies.php
@@ -3,17 +3,31 @@
 
 $container = $app->getContainer();
 
-// view renderer
-$container['renderer'] = function ($c) {
-    $settings = $c->get('settings')['renderer'];
-    return new Slim\Views\PhpRenderer($settings['template_path']);
+// // view renderer
+// $container['renderer'] = function ($c) {
+//     $settings = $c->get('settings')['renderer'];
+//     return new Slim\Views\PhpRenderer($settings['template_path']);
+// };
+
+// Register component on container
+$container['view'] = function ($container) {
+    $settings = $container->get('settings')['renderer'];
+    $view = new \Slim\Views\Twig($settings['template_path'], [
+        'cache' => $settings['cache_path']
+    ]);
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $container['router'],
+        $container['request']->getUri()
+    ));
+
+    return $view;
 };
 
 // monolog
-$container['logger'] = function ($c) {
-    $settings = $c->get('settings')['logger'];
-    $logger = new Monolog\Logger($settings['name']);
-    $logger->pushProcessor(new Monolog\Processor\UidProcessor());
-    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], Monolog\Logger::DEBUG));
-    return $logger;
-};
+// $container['logger'] = function ($c) {
+//     $settings = $c->get('settings')['logger'];
+//     $logger = new Monolog\Logger($settings['name']);
+//     $logger->pushProcessor(new Monolog\Processor\UidProcessor());
+//     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], Monolog\Logger::DEBUG));
+//     return $logger;
+// };
--- app/routes.php
+++ app/routes.php
@@ -1,10 +1,15 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+// $app->get('/[{name}]', function ($request, $response, $args) {
+//     // Sample log message
+//     $this->logger->info("Slim-Skeleton '/' route");
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+//     // Render index view
+//     return $this->renderer->render($response, 'index.phtml', $args);
+// });
+
+$app->get('/admin', function ($request, $response)
+{
+	return $this->view->render($response, 'login.twig');
 });
