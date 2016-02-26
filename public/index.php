95268d859ad334873b156bebf2887a06e44b8a34
Deepak <deeps.adhi@gmail.com>
2016-02-26 22:10:57 +0545

    code moved to bootstrap

--- public/index.php
+++ public/index.php
@@ -8,22 +8,6 @@ if (PHP_SAPI == 'cli-server') {
     }
 }
 
-// require __DIR__ . '/../vendor/autoload.php';
-
-// session_start();
-
-// // Instantiate the app
-// $settings = require __DIR__ . '/../config/app.php';
-// $app = new \Slim\App($settings);
-
-// // Set up dependencies
-// require __DIR__ . '/../app/dependencies.php';
-
-// // Register middleware
-// require __DIR__ . '/../app/middleware.php';
-
-// // Register routes
-// require __DIR__ . '/../app/routes.php';
 
 $app = require __DIR__ . '/../app/bootstrap.php';
 
