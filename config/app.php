c90273490aa509236d0ce9b3de9ffa2caa26f0fa
Deepak <deeps.adhi@gmail.com>
2016-02-27 00:12:53 +0545

    twig cache path defined

--- config/app.php
+++ config/app.php
@@ -6,7 +6,7 @@ return [
         // Renderer settings
         'renderer' => [
             'template_path' => __DIR__ . '/../templates/',
-            'cache_path   ' => __DIR__ . '/../cache/',
+            'cache_path'    => __DIR__ . '/../cache/',
         ],
 
         // Theme settings
