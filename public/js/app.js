d22b47a1d3a93ad6136ce99eb5b58a0d0f8c975f
Deepak <deeps.adhi@gmail.com>
2016-02-27 23:49:16 +0545

    file and path hidden input value able to change dynamically

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,10 @@
 $('#confirm-delete').on('show.bs.modal', function(e) {
     $(this).find('.btn-ok').attr('name', $(e.relatedTarget).data('name'));
-
     $('#name').html($(this).find('.btn-ok').attr('name'));
 
-    $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
-    $('#delete-form').attr("action", $(this).find('.btn-ok').attr('href'));
+    $(this).find('.btn-ok').attr('file', $(e.relatedTarget).data('file'));
+    $('#file').val($(this).find('.btn-ok').attr('file'));
+
+    $(this).find('.btn-ok').attr('path', $(e.relatedTarget).data('path'));
+    $('#path').val($(this).find('.btn-ok').attr('path'));
 });
