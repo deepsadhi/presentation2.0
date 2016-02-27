b814b5892896a0cb89aedde5c8875dde3e87cd71
Deepak <deeps.adhi@gmail.com>
2016-02-27 22:16:30 +0545

    code refine

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,12 +1,17 @@
 <?php
 
+error_reporting(E_ALL | E_STRICT);
+
 require __DIR__ . '/../vendor/autoload.php';
 
+use Slim\App;
+
+session_cache_limiter(false);
 session_start();
 
 // Instantiate the app
 $settings = require __DIR__ . '/../config/app.php';
-$app = new \Slim\App($settings);
+$app = new App($settings);
 
 // Set up dependencies
 require __DIR__ . '/../app/dependencies.php';
