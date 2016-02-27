4104195fa3e35bc50123e6888d7daf547c6ecff7
Deepak <deeps.adhi@gmail.com>
2016-02-27 22:17:59 +0545

    storing public path

--- public/index.php
+++ public/index.php
@@ -1,13 +1,19 @@
 <?php
-if (PHP_SAPI == 'cli-server') {
-    // To help the built-in PHP dev server, check if the request was actually for
-    // something which should probably be served as a static file
-    $file = __DIR__ . $_SERVER['REQUEST_URI'];
-    if (is_file($file)) {
-        return false;
-    }
-}
+// if (PHP_SAPI == 'cli-server') {
+//     // To help the built-in PHP dev server, check if the request was actually for
+//     // something which should probably be served as a static file
+//     $file = __DIR__ . $_SERVER['REQUEST_URI'];
+//     if (is_file($file))
+//     {
+//     	if (file_exists($file))
+//     	{
+//             die($file);
+//         	return false;
+//         }
+//     }
+// }
 
+define('PUBLIC_PATH', getcwd());
 
 $app = require __DIR__ . '/../app/bootstrap.php';
 
