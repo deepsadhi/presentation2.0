f6a76f75cd103534b96b803529e4c693a291379c
Deepak <deeps.adhi@gmail.com>
2016-02-28 21:48:27 +0545

    element id fixed

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 $('#confirm-delete').on('show.bs.modal', function(e) {
-    $(this).find('.btn-ok').attr('name', $(e.relatedTarget).data('name'));
-    $('#name').html($(this).find('.btn-ok').attr('name'));
+    $(this).find('.btn-ok').attr('file', $(e.relatedTarget).data('file'));
+    $('#name').html($(this).find('.btn-ok').attr('file'));
 
     $(this).find('.btn-ok').attr('file', $(e.relatedTarget).data('file'));
     $('#file').val($(this).find('.btn-ok').attr('file'));
