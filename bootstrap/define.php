2731b7b1001d1863b529ba58aaeeddea80016b43
Deepak <deeps.adhi@gmail.com>
2016-03-06 21:17:45 +0545

    code cleanup

--- bootstrap/define.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-defined('SQLITE_FILE') ?
-	null : define('SQLITE_FILE', APP_ROOT.'/config/app.db');
--- public_/admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-session_destroy();
-unset($_SESSION);
-session_regenerate_id(true);
-
-header('Location: index.php');
-exit();
